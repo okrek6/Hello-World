affc627a65f7bd06a7d4e7956252b333bb54a7ef
Brendan Krekeler <krekeler@Brendans-MacBook-Pro.local>
2018-06-06 23:59:48 -0500

    First commit

---
+++ Hello World/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Brendan Krekeler on 6/6/18.
+//  Copyright © 2018 Brendan Krekeler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
